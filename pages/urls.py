09663cbca0d7af2297e3d323ff4225bb94f67986
Sumit Hanchate <60186121+sumit-h@users.noreply.github.com>
2020-09-10 17:29:44 +0530

    added

--- pages/urls.py
+++ pages/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
 path('',views.home,name='home'),
+path('about',views.about,name='about'),
+path('services',views.services,name='services'),
+path('connect',views.contact,name='contact'),
+
 ]
