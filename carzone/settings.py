7315518284c578e9156bbe8b0742f033269a112b
Sumit Hanchate <60186121+sumit-h@users.noreply.github.com>
2020-09-11 16:51:32 +0530

    team model complete

--- carzone/settings.py
+++ carzone/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD':'sumit',
+        'HOST':'localhost'
     }
 }
 
@@ -123,3 +126,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
 os.path.join(BASE_DIR, 'carzone/static'),
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Team
+from django.utils.html import format_html
 # Register your models here.
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self,object):
+        return format_html('<img src="{}" width="40" style="border-radius:50px;"  />'.format(object.photo.url))
+# format(object.photo.url)) goes into src
+    thumbnail.short_description = 'photo'   # to change name thumbnail to ph
+    list_display = ('id','thumbnail','first_name','designation','craeted_date')
+    list_display_links = ('id','first_name','thumbnail')
+    search_fields = ('first_name','last_name')
+    list_filter = ('designation',)
+admin.site.register(Team,TeamAdmin)
