25eb0df6a550b41fbc733b9d7e5e37df6a5cd528
Sumit Hanchate <60186121+sumit-h@users.noreply.github.com>
2020-09-16 16:30:18 +0530

    adding values to home page

--- cars/views.py
+++ cars/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render , get_object_or_404
 from .models import Car
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
 def cars(request):
     cars = Car.objects.order_by('-created_date')
+
+    paginator = Paginator(cars,4)
+    page = request.GET.get('page')
+    paged_cars = paginator.get_page(page)
+
     data = {
-        'cars':cars,
+        'cars':paged_cars,
     }
     return render(request,'cars/cars.html',data)
 
@@ -14,3 +20,14 @@ def car_detail(request,id):
        'single_car':single_car,
     }
     return render(request,'cars/car_detail.html',data)
+
+def search(request):
+    cars = Car.objects.order_by('-created_date')
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            cars = cars.filter(description__icontains = keyword )
+    data = {
+    'cars':cars,
+    }
+    return render(request,'cars/search.html',data)
